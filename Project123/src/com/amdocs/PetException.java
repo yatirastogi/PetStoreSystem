517e1870a516838a14b96384e384474c2d62106d
yatirastogi <63449149+yatirastogi@users.noreply.github.com>
2023-09-12 09:43:48 +0530

    Update PetException.java

--- Project123/src/com/amdocs/PetException.java
+++ Project123/src/com/amdocs/PetException.java
@@ -5,6 +5,9 @@ public class PetException extends Exception {
 	PetException() {
 	     System.out.println("Pet not available");
 	   }
-	   
+	PetException(int a)
+	{
+		System.out.print("Please enter a positive integer");
+	}
 
 }
